9e2e00e52e351810cde98bfb17593de5a62cda1d
Subodhkumar <snp42u@gmail.com>
2026-08-05 21:03:36 +0530

    Add fruit input collection and display functionality

---
+++ Day2/practice.py
@@ -0,0 +1,12 @@
+fruits=[]
+
+fruit1 = input("Enter Fruit name: ")
+fruits.append(fruit1)
+fruit2 = input("Enter Fruit name: ")
+fruits.append(fruit2)
+fruit3 = input("Enter Fruit name: ")
+fruits.append(fruit3)
+fruit4 = input("Enter Fruit name: ")
+fruits.append(fruit4)
+
+print(fruits)
