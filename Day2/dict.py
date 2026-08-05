e71da2366b9d7817c6f1442e4627773cbdacbb20
Subodhkumar <snp42u@gmail.com>
2026-08-05 22:57:05 +0530

    refactor input handling and update set functionality

--- Day2/dict.py
+++ Day2/dict.py
@@ -12,17 +12,34 @@
 # print(marks)
 # print(marks.get("Subodh"))
 
-d = {}
+# d = {}
 
-word1 = input("Enter Hindi word: ")
-mean1 = input("Enter English meaning: ")
-word2 = input("Enter Hindi word: ")
-mean2 = input("Enter English meaning: ")
-word3 = input("Enter Hindi word: ")
-mean3 = input("Enter English meaning: ")
+# word1 = input("Enter Hindi word: ")
+# mean1 = input("Enter English meaning: ")
+# word2 = input("Enter Hindi word: ")
+# mean2 = input("Enter English meaning: ")
+# word3 = input("Enter Hindi word: ")
+# mean3 = input("Enter English meaning: ")
 
-d[word1]=mean1
-d[word2]=mean2
-d[word3]=mean3
+# d[word1]=mean1
+# d[word2]=mean2
+# d[word3]=mean3
 
-print(d)
+# print(d)
+
+s = set()
+
+num1 = int(input("Enter Hindi word: "))
+s.add(num1)
+num2 = int(input("Enter Hindi word: "))
+s.add(num2)
+num3 = int(input("Enter Hindi word: "))
+s.add(num3)
+num4 = int(input("Enter Hindi word: "))
+s.add(num4)
+num5 = int(input("Enter Hindi word: "))
+s.add(num5)
+num6 = int(input("Enter Hindi word: "))
+s.add(num6)
+
+print(s)
