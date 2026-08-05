391612411b652c725159d9988259b3c958937b62
Subodhkumar <snp42u@gmail.com>
2026-08-05 22:49:14 +0530

    add Hindi-English word input functionality

--- Day2/dict.py
+++ Day2/dict.py
@@ -1,13 +1,28 @@
-marks={
-    "Subodh":100,
-    "mayur":95,
-    "Rahul":90
-}
+# marks={
+#     "Subodh":100,
+#     "mayur":95,
+#     "Rahul":90
+# }
 
-print(marks, type(marks))
-print(marks.items())
-print(marks.keys())
-print(marks.values())
-marks.update({"Subodh": 99, "Suraj" : 98})
-print(marks)
-print(marks.get("Subodh"))
+# print(marks, type(marks))
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Subodh": 99, "Suraj" : 98})
+# print(marks)
+# print(marks.get("Subodh"))
+
+d = {}
+
+word1 = input("Enter Hindi word: ")
+mean1 = input("Enter English meaning: ")
+word2 = input("Enter Hindi word: ")
+mean2 = input("Enter English meaning: ")
+word3 = input("Enter Hindi word: ")
+mean3 = input("Enter English meaning: ")
+
+d[word1]=mean1
+d[word2]=mean2
+d[word3]=mean3
+
+print(d)
