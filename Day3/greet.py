be327333e775977466b8ec41dd4e708e94e8f376
Subodhkumar <snp42u@gmail.com>
2026-08-06 20:55:42 +0530

    Add greet and table scripts for greeting names and multiplication table generation

---
+++ Day3/greet.py
@@ -0,0 +1,5 @@
+l = ["Subodh", "Romeo", "Suraj", "Mayur", "soham"]
+
+for i in l:
+    if(i.startswith("S") or i.startswith("s")):
+        print(f"Hello {i}")
---
+++ Day3/table.py
@@ -0,0 +1,12 @@
+num = int(input("Enter na number: "))
+
+i = 0
+# for i in range(1,11):                 #Table using for loop
+#     print(f"{num} x {i} = {num*i}")
+
+
+
+while(i<11):
+    print(f"{num} x {i} = {num*i}")
+# for i in range(1,11):                 #Table using for loop
+    i += 1
