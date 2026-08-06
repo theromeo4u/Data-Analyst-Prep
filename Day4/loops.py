af8ec230a3da94de7b7a829b8f3daf3bacc9562d
Subodhkumar <snp42u@gmail.com>
2026-08-06 20:33:36 +0530

    Add loop examples for list iteration and range breaking

---
+++ Day4/loops.py
@@ -0,0 +1,12 @@
+l = [1,2,3,4,"Romeo", "Mayur", "Rahul"]
+
+i = 0
+while(i<len(l)):       #loop in list
+    print(l[i])
+    i += 1
+
+
+for i in range(25):
+    if(i==12):
+        break           #break the loop
+    print(i)
