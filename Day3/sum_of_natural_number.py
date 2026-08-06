6fb901a0a40bbcc6d70a5a665db33a133c5157b8
Subodhkumar <snp42u@gmail.com>
2026-08-06 21:53:21 +0530

    Add factorial and sum of natural numbers scripts

---
+++ Day3/sum_of_natural_number.py
@@ -0,0 +1,19 @@
+# num = int(input("Enter a num: "))
+# sum=0
+# for i in range(num+1):      #using for loop
+#     sum += i
+
+
+# print(sum) 
+
+
+
+num = int(input("Enter a num: "))
+sum=0
+i=0
+while(i<=num):                 #using while loop
+    sum += i
+    i+=1
+
+
+print(sum) 
