9e890d507a25c8e3a9253931975dc2d9e7644da9
Subodhkumar <snp42u@gmail.com>
2026-08-06 19:26:47 +0530

    Add username length checker and spam message detector scripts

---
+++ Day3/count.py
@@ -0,0 +1,13 @@
+userName = input("Enter user name: ")
+# length = len(userName)
+
+# if(length>=10):
+#     print("Username contains more than 10 character")
+# else:
+#     print("Username contains less than 10 character")
+
+
+if(len(userName)>=10):
+    print("Username contains more than 10 character")
+else:
+    print("Username contains less than 10 character")
---
+++ Day3/spam.py
@@ -0,0 +1,4 @@
+msg = input("Enter a message: ")
+
+if(msg=="Make a lot of money" or msg == "buy now" or msg == "subscribe this" or msg =="click this"):
+    print("Beware this is spam!!")
