a0cb94719d4527a2a65d6d68c876e1f248ca605b
Subodhkumar <snp42u@gmail.com>
2026-08-08 12:43:04 +0530

    Add pattern printing functionality to generate hollow rectangle

--- Day3/pattern.py
+++ Day3/pattern.py
@@ -1,7 +1,53 @@
 n = int(input("Enter a num: "))
 
 
+# for i in range(1,n+1):
+#     print(" " * (n-i),end="")
+#     print("*" * (2*i-1),end="")
+#     print("")
+
+'''
+    Output:
+
+    *
+   ***
+  *****
+
+'''
+
+
+# for i in range(1,n+1):
+#     # print(" " * (n-i),end="")
+#     print("*" * i,end="")
+#     print("")
+
+
+
+#     '''
+#     Output:
+
+    
+# *
+# **
+# ***
+
+# '''
+
 for i in range(1,n+1):
-    print(" " * (n-i),end="")
-    print("*" * (2*i-1),end="")
-    print("")
+    if(i==1 or i==n):
+        print("*"* n, end="")
+    else:
+        print("*", end="")
+        print(" "* (n-2),end="")
+        print("*", end="")
+print("")
+
+'''
+Output:
+
+    
+*
+**
+***
+
+'''
