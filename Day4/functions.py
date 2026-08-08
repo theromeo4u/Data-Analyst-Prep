9c24cb32f34ca2b1befc1c99f1f169bddb7d1fa8
Subodhkumar <snp42u@gmail.com>
2026-08-08 12:55:26 +0530

    Add average calculation functionality for user inputs

---
+++ Day4/functions.py
@@ -0,0 +1,14 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+average = (a+b+c)/3
+print(average)
+
+print("Thank You!!")
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+average = (a+b+c)/3
+print(average)
