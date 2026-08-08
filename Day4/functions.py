24058ab2dd866aa7a076025ce3ce41157f3a50bb
Subodhkumar <snp42u@gmail.com>
2026-08-08 12:56:36 +0530

    Refactor average calculation to encapsulate logic within a function

--- Day4/functions.py
+++ Day4/functions.py
@@ -1,14 +1,28 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter a number: "))
-c = int(input("Enter a number: "))
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# c = int(input("Enter a number: "))
 
-average = (a+b+c)/3
-print(average)
+# average = (a+b+c)/3
+# print(average)
 
-print("Thank You!!")
-a = int(input("Enter a number: "))
-b = int(input("Enter a number: "))
-c = int(input("Enter a number: "))
+# print("Thank You!!")
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# c = int(input("Enter a number: "))
 
-average = (a+b+c)/3
-print(average)
+# average = (a+b+c)/3
+# print(average)
+
+
+def average():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    c = int(input("Enter a number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+
+average()
+average()
+average()
